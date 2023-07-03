57acec283541850543e7bb41b9a71162303b6678
Pridesd <whwotjr98@naver.com>
2023-07-03 23:39:49 +0900

    refac: 파일명 변경

--- umc-front/src/App.js
+++ umc-front/src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/layouts/Headerco.jsx';
-import Main from './pages/main/Main.jsx';
-import LawyerIntroduction from './pages/lawyer/LawyerPage.jsx';
-import ListDetail from './pages/listDetail/ListDetailPage.jsx';
-import ListWrite from './pages/list/ListPage.jsx';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layouts/Headerco.jsx";
+import Main from "./pages/main/Main.jsx";
+import LawyerIntroduction from "./pages/lawyer/LawyerPage.jsx";
+import ListDetail from "./pages/listDetail/ListDetailPage.jsx";
+import ListWrite from "./pages/list/ListWrite.jsx";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-
-      <Route exact path="/" element={<Main/>}  />
-      <Route path="/lawyer-introduction" element={<LawyerIntroduction/>} />
-      <Route path="/listDetail" element={<ListDetail/>} />
-      <Route path="/listWrite" element={<ListWrite/>} />
-
+        <Route exact path="/" element={<Main />} />
+        <Route path="/lawyer-introduction" element={<LawyerIntroduction />} />
+        <Route path="/listDetail" element={<ListDetail />} />
+        <Route path="/listWrite" element={<ListWrite />} />
       </Routes>
     </Router>
   );
