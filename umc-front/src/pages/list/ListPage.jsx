b7d0be40047457276eefe7270699b5abcb852168
Pridesd <whwotjr98@naver.com>
2023-07-03 21:42:24 +0900

    feat: input state사용

--- umc-front/src/pages/list/ListPage.jsx
+++ umc-front/src/pages/list/ListPage.jsx
@@ -1,4 +1,6 @@
+import { useRef, useState } from "react";
 import styled from "styled-components";
+import CATEGORIES from "../../constants/categories";
 
 const Container = styled.div`
   min-width: 100vw;
@@ -14,14 +16,87 @@ const EditContainer = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   box-sizing: border-box;
   padding: 5rem 10rem;
+  gap: 1.725rem;
 `;
 
 const EditDesc = styled.div`
+  display: flex;
+  width: 60vw;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
   p {
     font-weight: 700;
+    margin: 0;
   }
+`;
+
+const InputContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  gap: 2rem;
+  width: 100%;
+`;
+
+const RadioBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.725rem;
+`;
+
+const Radio = styled.div`
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 10000px;
+
+  .select {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  label {
+    font-weight: 600;
+    margin-bottom: 0.725rem;
+  }
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  height: 2rem;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  width: 100%;
+  outline: none;
+`;
+
+const Textarea = styled.textarea`
+  box-sizing: border-box;
+  width: 100%;
+  min-height: 10rem;
+  resize: none;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  padding: 1rem;
+
+  outline: none;
+`;
+
+const Blur = styled.span`
+  opacity: 0.7;
 `;
 
 const Yellow = styled.span`
@@ -29,6 +104,27 @@ const Yellow = styled.span`
 `;
 
 const ListWrite = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const currentRef = useRef(null);
+
+  const changeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const changeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const selectCategory = (e) => {
+    if (currentRef.current === e.target) return;
+    // console.log()
+    // currentRef.current.classList.remove("select");
+    // currentRef.current = e.target;
+    e.target.classList.add("select");
+  };
+
   return (
     <Container>
       <EditContainer>
@@ -38,6 +134,45 @@ const ListWrite = () => {
             <Yellow>무료로</Yellow> 변호사 답변을 받으세요
           </p>
         </EditDesc>
+        <InputContainer>
+          <RadioBox>
+            {CATEGORIES.map((category) => (
+              <Radio key={category} onClick={selectCategory}>
+                {category}
+              </Radio>
+            ))}
+          </RadioBox>
+          <InputBox>
+            <label htmlFor="title">
+              제목{" "}
+              <Blur>
+                (10자 이상<Yellow>*</Yellow>)
+              </Blur>
+            </label>
+            <Input
+              value={title}
+              onChange={changeTitle}
+              id="title"
+              type="text"
+              placeholder="제목을 작성해주세요"
+            />
+          </InputBox>
+          <InputBox>
+            <label htmlFor="content">
+              내용{" "}
+              <Blur>
+                (200자 이상<Yellow>*</Yellow>)
+              </Blur>
+            </label>
+            <Textarea
+              name="content"
+              id="content"
+              placeholder="내용을 작성해주세요"
+              value={content}
+              onChange={changeContent}
+            />
+          </InputBox>
+        </InputContainer>
       </EditContainer>
     </Container>
   );
