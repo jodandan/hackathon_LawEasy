e146d6973673d59cc72fb442da3e355ba94495f4
jodan <wdd789q@naver.com>
2023-07-03 23:26:27 +0900

    feat : 변호사 소개 페이지 완성

--- umc-front/src/pages/lawyer/LawyerProfile.jsx
+++ umc-front/src/pages/lawyer/LawyerProfile.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const ProfileGrid = styled.div`
+  padding-top: 30px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 1fr); /* Add an additional column for the divider */
   grid-gap: 20px;
   margin: 0px 120px;
   padding-bottom: 10px;
@@ -12,46 +13,66 @@ const ProfileGrid = styled.div`
 const ProfileItem = styled.div`
   border: 1px solid #ccc;
   padding: 10px;
-  border: none; /* 경계선 제거 */
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  grid-column-gap: 0px;
 `;
 
 const VerticalDivider = styled.div`
-  width: 1px; /* 세로 경계선의 너비 */
-  background-color: #ccc; /* 세로 경계선의 색상 */
-  height: 100%; /* 그리드 아이템의 높이와 동일하게 설정 */
-  grid-column: 1 / span 2; /* 아이템이 두 개 열을 가로지르도록 설정 */
+  width: 1px; /* width of vertical border */
+  background-color: #ccc; /* vertical border color */
+  height: 100%; /* Set it equal to the height of the grid item */
+  grid-column: 2; /* Set the divider to the second column */
+`;
+
+const CounselingCell = styled.div`
+  border: 1px solid #ccc;
+  padding: 5px;
+  padding-right: 5px;
+`;
+
+const AdditionalInfo = styled.span`
+  font-size: 6px;
+  color: gray;
+  /* Add any additional styling properties you need */
 `;
 
 const LawyerProfile = () => {
   const profiles = [
     {
-      nickname2 : "해결사",
+      nickname2: "해결사",
       nickname: "쉽고 친절한 변호사",
       name: "ㅇㅇㅇ변호사",
       introduction: "나는야 변호사",
-      price: "$50/hour",
+      phonePrice: "$5/minute", // Phone consultation price
+      videoPrice: "$10/minute", // Video consultation price
     },
     {
-      nickname2 : "해결사",
-      nickname: "쉽고 친절한 변호사",
-      name: "ㅃㅃㅃ변호사",
-      introduction: "나는야 변호사",
-      price: "$60/hour",
-    },
-    {
-      nickname2 : "해결사",
-      nickname: "쉽고 친절한 변호사",
-      name: "ㅁㅁㅁ변호사",
-      introduction: "나는야 변호사",
-      price: "$70/hour",
-    },
-    {
-      nickname2 : "해결사",
-      nickname: "쉽고 친절한 변호사",
-      name: "ㄹㄹㄹ변호사",
-      introduction: "나는야 변호사",
-      price: "$80/hour",
-    },
+        nickname2: "해결사",
+        nickname: "쉽고 친절한 변호사",
+        name: "ㅇㅇㅇ변호사",
+        introduction: "나는야 변호사",
+        phonePrice: "$5/minute", // Phone consultation price
+        videoPrice: "$10/minute", // Video consultation price
+      },
+      {
+        nickname2: "해결사",
+        nickname: "쉽고 친절한 변호사",
+        name: "ㅇㅇㅇ변호사",
+        introduction: "나는야 변호사",
+        phonePrice: "$5/minute", // Phone consultation price
+        videoPrice: "$10/minute", // Video consultation price
+      },
+      {
+        nickname2: "해결사",
+        nickname: "쉽고 친절한 변호사",
+        name: "ㅇㅇㅇ변호사",
+        introduction: "나는야 변호사",
+        phonePrice: "$5/minute", // Phone consultation price
+        videoPrice: "$10/minute", // Video consultation price
+      },
+    // ...other profiles
   ];
 
   return (
@@ -59,12 +80,27 @@ const LawyerProfile = () => {
       {profiles.map((profile, index) => (
         <>
           <ProfileItem key={index}>
-          <p>{profile.nickname},{profile.nickname2}</p>
+            <p>
+                {profile.nickname},{profile.nickname2}
+                <p>
+                    <AdditionalInfo>추가 정보가 여기에 들어갑니다</AdditionalInfo>
+                </p>
+            </p>
+            
             <h3>{profile.name}</h3>
-            <p>소개: {profile.introduction}</p>
-            <p>가격: {profile.price}</p>
+            <p>Introduction: {profile.introduction}</p>
+            <div>
+              {/* Phone counseling cell */}
+              <CounselingCell>
+                Phone Counseling: {profile.phonePrice}
+              </CounselingCell>
+              {/* Video counseling cell */}
+              <CounselingCell>
+                Video Counseling: {profile.videoPrice}
+              </CounselingCell>
+            </div>
           </ProfileItem>
-          {/* 가운데 세로로 경계선 */}
+          {/* Center vertical border */}
           {index === 1 && <VerticalDivider />}
         </>
       ))}
