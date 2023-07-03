f5c4b9721bd83cc7303f42a9a610b2884ddfca59
Pridesd <whwotjr98@naver.com>
2023-07-03 19:43:21 +0900

    feat: edit 페이지 제작

--- umc-front/src/App.js
+++ umc-front/src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes } from "react-router-dom";
-import styled from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Edit from "./pages/post/Edit";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>헤더</div>
-      <Routes></Routes>
+      <div>헤더 부분</div>
+      <Routes>
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
     </BrowserRouter>
   );
 }
